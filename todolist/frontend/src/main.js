065796140a57f3ec9ec5aa0f46750fcd4cd9629d
Ste003 <tommasostefani3@gmail.com>
2025-02-12 21:36:38 +0100

    azzeramento progetto

--- todolist/frontend/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';  // Import del router
-
-createApp(App)
-  .use(router)  // Usa il router
-  .mount('#app');
