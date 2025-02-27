172f43c6723cc7ca2b1b454c7e2730a594205794
Ste003 <tommasostefani3@gmail.com>
2025-02-27 15:00:39 +0100

    aggiornamento cors

--- todo/src/main/java/com/todoList/todo/config/CorsConfig.java
+++ todo/src/main/java/com/todoList/todo/config/CorsConfig.java
@@ -14,11 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:5173") // URL del frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") // Metodi permessi
+                        .allowCredentials(true) // Permette di inviare i cookie (ad esempio il token di sessione)
+                        .allowedHeaders("*");  // Permetti tutti gli header
             }
         };
     }
 }
-
