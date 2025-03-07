4a9388e10d25c68db45e08ddf17b5c9c9670a2c6
Ste003 <tommasostefani3@gmail.com>
2025-03-07 23:05:11 +0100

    piccole aggiunte

--- todo/src/main/java/com/todoList/todo/service/TelegramNotificationService.java
+++ todo/src/main/java/com/todoList/todo/service/TelegramNotificationService.java
@@ -1,7 +1,20 @@
 package com.todoList.todo.service;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.core5.http.config.Registry;
 import org.apache.hc.core5.http.config.RegistryBuilder;
 import org.apache.hc.core5.ssl.SSLContexts;
@@ -14,26 +27,14 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import jakarta.annotation.PostConstruct;
 
-import javax.net.ssl.SSLContext;
-
-import java.security.KeyStore;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
-
-import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
-import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
-import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
-
 @Service
 public class TelegramNotificationService {
 
     @Value("${telegram.bot.token}")
     private String botToken;
+
+    @Value("${percorso.java.cacerts}")
+    private String percorsoJava;
 
     private RestTemplate restTemplate;
 
@@ -44,9 +45,9 @@ public class TelegramNotificationService {
 
     private RestTemplate createSecureRestTemplate() {
         try {
-            // Carica il truststore contenente il certificato importato
+            // Carica il truststore usando il percorso specificato nella proprietà
             KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
-            try (FileInputStream trustStoreStream = new FileInputStream("C:/Program Files/Java/jdk-23/lib/security/cacerts")) {
+            try (FileInputStream trustStoreStream = new FileInputStream(percorsoJava)) {
                 trustStore.load(trustStoreStream, "changeit".toCharArray());
             }
 
@@ -55,26 +56,28 @@ public class TelegramNotificationService {
                     .loadTrustMaterial(trustStore, (X509Certificate[] chain, String authType) -> true)
                     .build();
 
-            // Crea un SSLConnectionSocketFactory con l'SSLContext
+            // Costruisci un SSLConnectionSocketFactory e un HttpClient con questo
+            // SSLContext
             @SuppressWarnings("deprecation")
             SSLConnectionSocketFactory sslSocketFactory = new SSLConnectionSocketFactory(sslContext);
-
-            // Crea un registry e registra lo SSLConnectionSocketFactory
             @SuppressWarnings("deprecation")
-            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+            Registry<org.apache.hc.client5.http.socket.ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<org.apache.hc.client5.http.socket.ConnectionSocketFactory>create()
                     .register("https", sslSocketFactory)
                     .build();
 
-            // Crea un HttpClient con SSL configurato
             @SuppressWarnings("deprecation")
+            PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(
+                    socketFactoryRegistry);
             CloseableHttpClient httpClient = HttpClients.custom()
-                    .setConnectionManager(new PoolingHttpClientConnectionManager(socketFactoryRegistry))
+                    .setConnectionManager(connectionManager)
                     .build();
 
-            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+                    httpClient);
             return new RestTemplate(requestFactory);
 
-        } catch (IOException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+        } catch (IOException | KeyManagementException | KeyStoreException | NoSuchAlgorithmException
+                | CertificateException e) {
             throw new RuntimeException("Errore nella configurazione SSL per RestTemplate", e);
         }
     }
